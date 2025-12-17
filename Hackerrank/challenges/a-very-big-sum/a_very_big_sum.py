4e842850c0aef584617686ce7ef8587c0fb8ee90
Ahmad  Al Dibo <ahmadaldibo212009@gmail.com>
2025-12-17 11:04:30 +0100

    Create a_very_big_sum.py

---
+++ Hackerrank/challenges/a-very-big-sum/a_very_big_sum.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'aVeryBigSum' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER_ARRAY ar as parameter.
+#
+
+def aVeryBigSum(ar):
+    for i in ar:
+        assert 1<= len(ar) <= 10
+        assert 0 <= i <= 10**10
+    return sum(ar)
+
